62e71c84903a5aea58a4d14c10cbbe0cb00b187b
Tom Koptel <tom.koptel@gmail.com>
2018-02-11 20:41:58 +0100

    Implement solution for ticket

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char c;
+    int s = 0;
+
+    while (scanf("%c", &c), c != '\n') {
+        // We take number one by one and sum them. It is not stated clearly in the task that we need convert input numbers to sum up.
+        // https://stackoverflow.com/questions/15598698/why-does-subtracting-0-in-c-result-in-the-number-that-the-char-is-representing
+        s += c - '0';
+    }
+
+    printf(s % 3 == 0 ? "YES\n" : "NO\n");
     return 0;
 }
