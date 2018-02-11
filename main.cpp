324e8e32cf0f0a8288d568736120d1f8bbd7244f
Tom Koptel <tom.koptel@gmail.com>
2018-02-11 18:00:34 +0100

    Add solution for 'Brackets' task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
+using namespace std;
+
+/**
+ * The task https://www.e-olymp.com/uk/contests/5553/problems/43468
+ *
+ * Input: ((())())
+ * Output: YES
+ *
+ * Input: (()
+ * Output: NO
+ *
+ * @return 0
+ */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string input;
+    cin >> input;
+
+    int x = 0;
+    for (char i : input) {
+        if (i == ')') {
+            x--;
+            if (x < 0) {
+                cout << "NO" << endl;
+                return 0;
+            }
+        } else {
+            x++;
+        }
+    }
+
+    if (x == 0) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
     return 0;
 }
