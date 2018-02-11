f39588824a8dda4587c3dc163c9198753eda3993
Tom Koptel <tom.koptel@gmail.com>
2018-02-11 17:58:34 +0100

    Add solution for 'Robot' task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 
+using namespace std;
+
+/**
+ * The source of task https://www.e-olymp.com/uk/contests/5553/problems/43457
+ *
+ * Input: RRSRRLRR
+ * Output: 6
+ *
+ * @return 0
+ */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int min = 10000, max = 10000, x = 10000;
+    string input;
+    cin >> input;
+
+    for (char i : input) {
+        if (i == 'L') {
+            x--;
+            if (x < min) {
+                min = x;
+            }
+        } else if (i == 'R') {
+            x++;
+            if (x > max) {
+                max = x;
+            }
+        }
+    }
+
+    cout << max - min + 1 << endl;
+
     return 0;
 }
