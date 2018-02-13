d0cfb6de563eb12508d8a5916e25519429064d48
Tom Koptel <tom.koptel@gmail.com>
2018-02-13 22:14:03 +0100

    Solving replace digit task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+using namespace std;
+
+/**
+ * The issue: https://www.e-olymp.com/ru/contests/5553/problems/43464
+ * Input: 123118 1 5 Output: 523558
+ *
+ * @return 0
+ */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int number, a, b;
+    scanf("%d %d %d", &number, &a, &b);
+
+    string numberAsString = to_string(number);
+    string aAsString = to_string(a);
+    string bAsString = to_string(b);
+
+    while (numberAsString.find(aAsString) != string::npos) {
+        numberAsString.replace(numberAsString.find(aAsString), aAsString.length(), bAsString);
+    }
+
+    cout << numberAsString << endl;
+
     return 0;
 }
