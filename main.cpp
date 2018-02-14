2038170bb88087e325e92e94dae3a1bf5ff9948f
Tom Koptel <tom.koptel@gmail.com>
2018-02-14 21:20:16 +0100

    Solve the task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+using namespace std;
+
+/**
+ * The source: https://www.e-olymp.com/ru/problems/6827
+ *
+ * @return 0
+ */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string doctorsPhrase;
+    string johnsPhrase;
+
+    cin >> doctorsPhrase;
+    cin >> johnsPhrase;
+
+    if (johnsPhrase.length() > doctorsPhrase.length()) {
+        cout << "no" << endl;
+    } else {
+        cout << "go" << endl;
+    }
+
     return 0;
 }
