ce359b28a22d5ed713478eaddf242c5ecfae03f6
Tom Koptel <tom.koptel@gmail.com>
2018-02-14 20:38:14 +0100

    Half solved task about water

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+using namespace std;
+
+/**
+ * The source: https://www.e-olymp.com/ru/problems/3735
+ * Input: 11 Output: 5
+ *
+ * @return 0
+ */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    cin >> n;
+
+    int k = n % 6;
+    cout << ((k == 0) ? 0 : k) << endl;
+
     return 0;
 }
