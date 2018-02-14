64c6bdbfbb322390bb05cec2498349ac29c9eca4
Tom Koptel <tom.koptel@gmail.com>
2018-02-14 21:41:41 +0100

    Solving the task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
 
+using namespace std;
+
+/**
+ * The source https://www.e-olymp.com/ru/problems/462
+ *
+ * Input:
+ * 5
+ * 1 50 3 4 3
+ * 16
+ * 1 2 3 4 5 1 3 3 4 5 5 5 5 5 4 5
+ *
+ * Output:
+ * yes
+ * no
+ * no
+ * no
+ * yes
+ *
+ * @return 0
+ */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int numberOfKeys, totalNumberOfPresses;
+    cin >> numberOfKeys;
+
+    auto * numberOfPresses = new int[numberOfKeys];
+    for (int i = 0; i < numberOfKeys; ++i) {
+        cin >> numberOfPresses[i];
+    }
+    cin >> totalNumberOfPresses;
+    auto * keysPressed = new int[totalNumberOfPresses];
+    for (int i = 0; i < totalNumberOfPresses; ++i) {
+        cin >> keysPressed[i];
+    }
+
+    auto * recordingSequence = new int[numberOfKeys];
+    for (int j = 0; j < totalNumberOfPresses; ++j) {
+        recordingSequence[keysPressed[j] - 1]++;
+    }
+
+    for (int k = 0; k < numberOfKeys; ++k) {
+        int canBePressed = numberOfPresses[k];
+        int wasPressed = recordingSequence[k];
+
+        cout << ((wasPressed > canBePressed) ? "yes" : "no") << endl;
+    }
+
     return 0;
 }
