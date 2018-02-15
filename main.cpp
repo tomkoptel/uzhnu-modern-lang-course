35aead3b74bd8cf687960a95a23f7e0e20084ee8
Tom Koptel <tom.koptel@gmail.com>
2018-02-15 09:06:35 +0100

    Solving negative numbers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int size;
+    cin >> size;
+
+    auto *array = new float[size];
+    for (int i = 0; i < size; ++i) {
+        cin >> array[i];
+    }
+
+    float sum = 0;
+    int numberOfSeekItems = 0;
+    for (int j = 0; j < size; ++j) {
+        float element = array[j];
+        if (element < 0) {
+            sum += element;
+            numberOfSeekItems++;
+        }
+    }
+
+    printf("%d %.2f", numberOfSeekItems, sum);
+
     return 0;
 }
