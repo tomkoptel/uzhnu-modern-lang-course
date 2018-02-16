b187d37d1bf23dd5481c3e0bdb6272d76a8058b0
Tom Koptel <tom.koptel@gmail.com>
2018-02-16 18:37:33 +0100

    Solving swaping to right by 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 
+using namespace std;
+
+/**
+ * The source: https://www.e-olymp.com/ru/problems/922
+ *
+ * Input: 1 2 3 4
+ * Output: 4 1 2 3
+ *
+ * @return 0
+ */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int size;
+    cin >> size;
+
+    auto *array = new int[size];
+    auto *newArray = new int[size];
+    for (int i = 0; i < size; ++i) {
+        cin >> array[i];
+    }
+
+    for (int j = 0; j < size; ++j) {
+        int currentElement = array[j];
+        int moveToIndex = (j + 1 == size) ? 0 : j + 1;
+        newArray[moveToIndex] = currentElement;
+    }
+
+    for (int k = 0; k < size; ++k) {
+        cout << newArray[k] << " ";
+    }
+
+
     return 0;
 }
