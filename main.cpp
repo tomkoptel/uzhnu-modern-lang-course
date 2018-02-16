7c1c2003548ecde83ddce2fe806eb81b5cd66e66
Tom Koptel <tom.koptel@gmail.com>
2018-02-16 21:59:07 +0100

    Solving height related question

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 
+using namespace std;
+
+/**
+ * The source: https://www.e-olymp.com/ru/contests/5554/problems/43482
+ *
+ * @return
+ */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int size;
+
+    while (scanf("%d", &size) == 1) {
+        auto *array = new int[size];
+        for (int i = 0; i < size; ++i) {
+            scanf("%d", &array[i]);
+        }
+
+        int a, b;
+        scanf("%d %d", &a, &b);
+
+        int counter = 0;
+        for (int k = 0; k < size; ++k) {
+            int el = array[k];
+            if (el >= a && el <= b) {
+                counter++;
+            }
+        }
+
+        cout << counter << endl;
+    }
     return 0;
 }
