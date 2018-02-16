20d60ed4652c90ed9fe9076dd2597c8976ff67f9
Tom Koptel <tom.koptel@gmail.com>
2018-02-16 18:44:10 +0100

    Solving toy task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 
+using namespace std;
+
+/**
+ * The source: https://www.e-olymp.com/ru/submissions/submit?problem=927
+ *
+ * Input:
+ * 2 100.00
+ * 5 23.00
+ * 10 22.50
+ *
+ * Output: 15
+ *
+ * @return 0
+ */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int num;
+    cin >> num;
+
+    int total = 0;
+    for (int i = 0; i < num; ++i) {
+        int numOfToys;
+        float price;
+        scanf("%d %f", &numOfToys, &price);
+
+        if (price < 50) {
+            total += numOfToys;
+        }
+    }
+
+    cout << total << endl;
+
     return 0;
 }
