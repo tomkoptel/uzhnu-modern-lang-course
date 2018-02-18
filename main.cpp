9acbc2b4c380502a3366b63714a0d0f73e2f88e9
Tom Koptel <tom.koptel@gmail.com>
2018-02-18 18:44:54 +0100

    Solving singing class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    while (cin) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+
+        int result = gcd(a, b);
+        cout << "GCD " << result << endl;
+        printf("%s\n", (result == 1) ? "YES" : "NO");
+    }
     return 0;
 }
