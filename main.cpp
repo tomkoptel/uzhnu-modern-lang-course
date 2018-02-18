8c99ed7b6e7bb5b38e768124f9b4a206a44ad069
Tom Koptel <tom.koptel@gmail.com>
2018-02-18 19:04:23 +0100

    Solving the task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+using namespace std;
+
+long gcd(long a, long b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+/**
+ * In: 2 4
+ * Out: 1 2
+ *
+ * In: -12 15
+ * Out: -4 5
+ *
+ * @return
+ */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    long a, b;
+    cin >> a >> b;
+
+    long gcdVal = gcd(max(a, b), min(a, b));
+    a /= gcdVal;
+    b /= gcdVal;
+
+    cout << a << " " << b << endl;
     return 0;
 }
