873a374051562955f144de7c54c96aa957e6d6a5
Tom Koptel <tom.koptel@gmail.com>
2018-02-18 18:24:24 +0100

    Solving the rational number calc

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
+using namespace std;
+
+long gcd(long a, long b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+long lcm(long a, long b) {
+    long gcdVal = gcd(a, b);
+    return gcdVal ? a * b / gcdVal : 0;
+}
+
+/**
+ * In: 4 9 5 7
+ * Out: 73 63
+ *
+ * In: 2 3 -1 6
+ * Out: 3 6
+ *
+ * @return 0
+ */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    long a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    long lower = lcm(b, d);
+    long upper = (lower * a / b) + (lower * c / d);
+
+    long gcdResult = gcd(upper, lower);
+    if (gcdResult != 0) {
+        upper /= gcdResult;
+        lower /= gcdResult;
+    }
+
+    cout << upper << " " << lower << endl;
+
     return 0;
 }
