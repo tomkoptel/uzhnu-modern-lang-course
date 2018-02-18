02af4a4f27b764b7c63ac261e4d803a7387fd3df
Tom Koptel <tom.koptel@gmail.com>
2018-02-18 19:10:43 +0100

    Solving the task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+/**
+ * In: 2 1
+ * Out: 1
+ *
+ * @return
+ */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a, b;
+    scanf("%d %d", &a, &b);
+    cout << gcd(a, b) << endl;
     return 0;
 }
