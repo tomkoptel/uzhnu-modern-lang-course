26369db4ff62cbda37646464b55a7401ea6051a1
Tom Koptel <tom.koptel@gmail.com>
2018-02-18 16:00:40 +0100

    Solving LCM of array

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    int gcdResult = gcd(a, b);
+    return gcd ? (a / gcdResult * b) : 0;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int size;
+    cin >> size;
+
+    auto *array = new int[size];
+    for (int i = 0; i < size; ++i) {
+        scanf("%d", &array[i]);
+    }
+
+    int a = array[0];
+    for (int i = 1; i < size; ++i) {
+        int b = array[i];
+        a = lcm(a, b);
+    }
+
+    cout << a << endl;
+
     return 0;
 }
