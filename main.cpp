73b84be755a75e12fdc81dc5021bcfeb6df97a94
Tom Koptel <tom.koptel@gmail.com>
2018-02-18 19:18:22 +0100

    Solving the task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
+long gcd(long x, long y) {
+    while (x != 0 && y != 0) {
+        if (x > y) x = x % y;
+        else y = y % x;
+    }
+    return x + y;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    long a, b;
+    cin >> a >> b;
+    cout << gcd(a, b) << endl;
     return 0;
 }
