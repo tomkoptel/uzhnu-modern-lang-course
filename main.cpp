6ddf5bf5c852aadbc3b3ae012960c0a466b44a9e
Tom Koptel <tom.koptel@gmail.com>
2018-02-18 18:51:19 +0100

    Solving the task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+/**
+ * In: 125 80 67835 1000 25
+ * Out: 5
+ *
+ * @return
+ */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int size;
+    cin >> size;
+
+    auto * array = new int[size];
+    for (int i = 0; i < size; ++i) {
+        cin >> array[i];
+    }
+
+    int a = array[0];
+    for (int i = 0; i < size; ++i) {
+        int b = array[i];
+        a = gcd(a, b);
+    }
+
+    cout << a << endl;
+
     return 0;
 }
