207be1505f9dfb380109efd7d0c41cad27bdced2
Tom Koptel <tom.koptel@gmail.com>
2018-02-18 17:55:08 +0100

    Solving the task about mega GCD

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string tmp;
+    getline(cin, tmp);
+    vector<int> nums;
+    stringstream ss(tmp);
+
+    int num;
+    while (ss >> num) {
+        nums.push_back(num);
+    }
+
+    int a = nums[0];
+    for (int i = 1; i < nums.size(); ++i) {
+        int b = nums[i];
+        a = gcd(min(a, b), max(a, b));
+    }
+    cout << a << endl;
+
     return 0;
 }
