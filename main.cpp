4efa7607f5907bd22609aea74c86a815c742d780
Tom Koptel <tom.koptel@gmail.com>
2018-02-18 17:15:17 +0100

    Calculating max GCD for multiple arrays

--- main.cpp
+++ main.cpp
@@ -1,6 +1,63 @@
 #include <iostream>
+#include <vector>
 
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int maxGcd() {
+    vector<int> array;
+    char c;
+    int number;
+    do {
+        scanf("%d", &number);
+        array.push_back(number);
+    } while (scanf("%c", &c), c != '\n');
+
+    int maxVal = 0;
+    if (!array.empty()) {
+        for (int i = 0; i < array.size(); ++i) {
+            for (int j = i + 1; j < array.size(); ++j) {
+                int a = array[i];
+                int b = array[j];
+                int gcdResult = gcd(a, b);
+
+                maxVal = max(maxVal, gcdResult);
+                cout << "Comparing: " << a << " and " << b << " gcd is " << gcdResult << endl;
+            }
+        }
+    }
+
+    return maxVal;
+}
+
+/**
+ *
+ * Input:
+ * 3
+ * 10 20 30 40
+ * 7 5 12
+ * 125 15 25
+ *
+ * Output:
+ * 20 1 25
+ *
+ * @return 0
+ */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int tries;
+    cin >> tries;
+    cin.get();
+
+    auto *results = new int[tries];
+    for (int i = 0; i < tries; ++i) {
+        results[i] = maxGcd();
+    }
+
+    for (int i = 0; i < tries; ++i) {
+        cout << results[i] << endl;
+    }
 }
