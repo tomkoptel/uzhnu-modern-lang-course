3d1f566a159660431a590dfd2cde54ee86cddb62
Tom Koptel <tom.koptel@gmail.com>
2018-02-18 15:52:19 +0100

    Add GCD and LCD impl

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 
+using namespace std;
+
+int gcd(int a, int b) {
+    int reminder;
+    while (b) {
+        reminder = a % b;
+        a = b;
+        b = reminder;
+    }
+    return a;
+}
+
+int gcd2(int a, int b) {
+    if (b == 0) return a;
+    return gcd2(b, a % b);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int size;
+    cin >> size;
+
+    auto *array = new int[size];
+    for (int i = 0; i < size; ++i) {
+        scanf("%d", &array[i]);
+    }
+
+    int a = array[0];
+    for (int i = 1; i < size; ++i) {
+        int b = array[i];
+        a = gcd2(a, b);
+    }
+
+    cout << a << endl;
+
     return 0;
 }
