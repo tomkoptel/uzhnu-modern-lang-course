9f16dcdf6179e17b68e71f57f95a0943c956e2f8
Tom Koptel <tom.koptel@gmail.com>
2018-02-19 22:07:58 +0100

    Init impl

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+using namespace std;
+
+long exponent(long x, long n, long m) {
+    if (n == 0) return 1;
+
+    if (n % 2 == 0) {
+        return (exponent((x * x) % m, n / 2, m)) % m;
+    } else {
+        return (x * exponent(x, n - 1, m)) % m;
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    long a, b, m;
+    cin >> a >> b >> m;
+
+    cout << exponent(a, b, m) << endl;
     return 0;
 }
