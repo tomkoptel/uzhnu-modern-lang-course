8626c88de28dc318801956796e9cc4bf95b94ade
Tom Koptel <tom.koptel@gmail.com>
2018-02-21 21:40:23 +0100

    Solving the disk number issue

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+using namespace std;
+
+/**
+ * Solution taken from https://site.ada.edu.az/~medv/acm/Docs%20e-olimp/Volume%2025/2415.htm
+ *
+ * @return 0
+ */
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    long n;
+    cin >> n;
+
+    long position = 0;
+    while ((n & 1) == 0) {
+        n /= 2;
+        position++;
+    }
+
+    cout << position << endl;
     return 0;
 }
