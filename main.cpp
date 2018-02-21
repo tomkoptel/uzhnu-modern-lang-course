0d91a4f4d35a5992274327c818b4f86a679644d2
Tom Koptel <tom.koptel@gmail.com>
2018-02-21 22:14:51 +0100

    Solving the task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+using namespace std;
+
+int hotpo(int n) {
+    if (n % 2) {
+        return n * 3 + 1;
+    } else {
+        return n >> 1;
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int tests;
+    cin >> tests;
+
+    while (tests--) {
+        int testNumber, n;
+        cin >> testNumber >> n;
+
+        int max = n;
+        while (n != 1) {
+            n = hotpo(n);
+            if (max < n) max = n;
+        }
+        printf("%d %d\n", testNumber, max);
+    }
     return 0;
 }
