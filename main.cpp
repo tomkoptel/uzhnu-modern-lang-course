92cd2f7dfe0db02c598dc36f286fc4978ce95c2c
Tom Koptel <tom.koptel@gmail.com>
2018-02-21 21:44:40 +0100

    Solving the task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+using namespace std;
+
+long fun10(long m, long n) {
+    if (m == 0 || m == n) return 1;
+    return fun10(m - 1, n - 1) + fun10(m, n - 1);
+}
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    long n, m;
+    cin >> n >> m;
+    cout << fun10(m, n) << endl;
     return 0;
 }
