671a3b4486f233221e9df0fedfaa6575947c1856
Tom Koptel <tom.koptel@gmail.com>
2018-02-21 21:59:47 +0100

    Solving the task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
+void hanoi(long n, long from, long to, long additional) {
+    if (n == 0) return;
+
+    hanoi(n - 1, from, additional, to);
+    cout << from << " " << to << endl;
+    hanoi(n - 1, additional, to, from);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    long n;
+    cin >> n;
+    hanoi(n, 1, 2, 3);
     return 0;
 }
