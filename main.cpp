3a448dcc8a4eca1970a74245f552318aa876acad
Tom Koptel <tom.koptel@gmail.com>
2018-02-23 14:31:25 +0100

    Solving the task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+using namespace std;
+
+void print(unsigned int n) {
+    std::bitset<4> bitSet = std::bitset<4>(n);
+    cout << bitSet.to_string() << endl;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    unsigned int n, res;
+    cin >> n;
+
+    print(n);
+    print(n - 1);
+
+    res = (n & (n - 1));
+
+    print(res);
+
+    printf("%d\n", res);
     return 0;
 }
