698eaae71aee3dc135803a9eb8d179022cd42baa
Tom Koptel <tom.koptel@gmail.com>
2018-02-23 14:58:58 +0100

    Solving the task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+using namespace std;
+
+void print(string a, unsigned int n) {
+    std::bitset<8> bitSet = std::bitset<8>(n);
+    cout << a << endl;
+    cout << bitSet.to_string() << endl;
+    cout << "----" << endl;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int res, k, n;
+
+    scanf("%d %d",&k,&n);
+
+    print("1 << k", 1 << k);
+    print("1 << n", 1 << n);
+
+    res = (1 << k) + (1 << n);
+    printf("%d\n", res);
+
     return 0;
 }
