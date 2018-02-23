07c016bdd9f9b89628f7ee6cfdbda0d41cddc4ca
Tom Koptel <tom.koptel@gmail.com>
2018-02-23 14:54:46 +0100

    Solving task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+using namespace std;
+
+void print(string a, unsigned int n) {
+    std::bitset<8> bitSet = std::bitset<8>(n);
+    cout << a << endl;
+    cout << bitSet.to_string() << endl;
+    cout << "----" << endl;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    unsigned int n;
+    cin >> n;
+
+    print("n", n);
+    print("n - 1", n - 1);
+    print("n & (n - 1)", n & (n - 1));
+    int res = n - (n & (n - 1));
+    print("n - (n & (n - 1))", res);
+
+    cout << res << endl;
+
     return 0;
 }
