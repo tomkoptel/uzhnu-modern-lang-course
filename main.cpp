673f491f51ded9daf8968b83a3ab313fae7d3643
Tom Koptel <tom.koptel@gmail.com>
2018-02-25 17:52:40 +0100

    Solving the Sieve of Eratosthenes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    cin >> n;
+
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+
+    for (int p = 2; p * p <= n; p++) {
+        if (prime[p]) {
+            for (int i = p * 2; i <= n; i += p) {
+                prime[i] = false;
+            }
+        }
+    }
+
+    for (int j = 2; j <= n; ++j) {
+        if (prime[j]) {
+            cout << j << " ";
+        }
+    }
+
     return 0;
 }
